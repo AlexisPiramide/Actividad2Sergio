293eacfd75181ed59e72d7be709b7166caf367d1
Alexis <220240@fppiramide.com>
2023-09-26 13:53:08 +0200

    Primer Comit

---
+++ src/test/java/com/example/Actividad2Sergio/Actividad2SergioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Actividad2Sergio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad2SergioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
